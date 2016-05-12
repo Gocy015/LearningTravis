25c311e04195e30867bb0c8bf124de0f5958c4c1
蔡阳 <Gocy@Gocy-mac.local>
2016-05-12 15:07:58 +0800

    update travis

--- LearningTravisTests/LearningTravisTests.swift
+++ LearningTravisTests/LearningTravisTests.swift
@@ -12,7 +12,7 @@ import XCTest
 class LearningTravisTests: XCTestCase {
     
     let vc = ViewController()
-    override func setUp() {
+    override func setUp(){
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
